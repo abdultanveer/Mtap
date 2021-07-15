d88625542cdd33ed8f8bf6ec6f7d26eb5a0845fe
abdul <abdul.tanveer@gmail.com>
2021-07-15 13:48:07 +0530

    Initial commit

---
+++ app/src/main/java/com/abdul/myfirsttap/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.abdul.myfirsttap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(view: View) {
+        Toast.makeText(this,"welcome to android",Toast.LENGTH_SHORT).show()
+    }
+
+
+}
