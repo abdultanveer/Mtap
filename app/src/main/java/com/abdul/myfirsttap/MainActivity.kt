e355db7f2f7af20f87e2b784de21a53c47dba112
abdul <abdul.tanveer@gmail.com>
2021-07-15 15:21:59 +0530

    getting text from edittext and showing it in a toast

--- app/src/main/java/com/abdul/myfirsttap/MainActivity.kt
+++ app/src/main/java/com/abdul/myfirsttap/MainActivity.kt
@@ -3,17 +3,21 @@ package com.abdul.myfirsttap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etName: EditText //declaration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etName = findViewById(R.id.etName) //initialization
     }
 
     fun clickHandler(view: View) {
-        Toast.makeText(this,"welcome to android",Toast.LENGTH_SHORT).show()
+        var name = etName.text.toString()
+        Toast.makeText(this,name,Toast.LENGTH_SHORT).show()
     }
 
 
