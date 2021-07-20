31a7a0078631f8dbcc8e9df08c6c322ea23056f1
abdul <abdul.tanveer@gmail.com>
2021-07-20 15:46:47 +0530

    lifecycle of an activity

--- app/src/main/java/com/abdul/myfirsttap/HomeActivity.kt
+++ app/src/main/java/com/abdul/myfirsttap/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.abdul.myfirsttap
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
@@ -10,13 +11,18 @@ import android.widget.Spinner
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
+    var TAG = HomeActivity::class.simpleName
     var countries = arrayOf("India","usa","uk")
     lateinit var tvDataReceived: TextView
     lateinit var etHome: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)  //inflating the layout xml
         etHome = findViewById(R.id.etHome)
+        Log.i(TAG,"onCreate")
+
 
         var adapter = ArrayAdapter<String>(this,
                                     android.R.layout.simple_list_item_1,
@@ -28,6 +34,37 @@ class HomeActivity : AppCompatActivity() {
         var receivedData =  intent.getStringExtra("mykey")
         tvDataReceived.text = receivedData*/
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"onStart--get location")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG,"onResume--- restore data")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,"onpause--- saving app state")
+
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"onstop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,"ondestroy")
+
+    }
+
 
     fun sendHandler(view: View) {
         var photoData = etHome.text.toString()
