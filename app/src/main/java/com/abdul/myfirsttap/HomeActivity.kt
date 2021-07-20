f75f58d1caa51e465ef3ebe0cf57c62af9f03f4d
abdul <abdul.tanveer@gmail.com>
2021-07-20 13:45:50 +0530

    array adapter on spinner

--- app/src/main/java/com/abdul/myfirsttap/HomeActivity.kt
+++ app/src/main/java/com/abdul/myfirsttap/HomeActivity.kt
@@ -4,16 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
+    var countries = arrayOf("India","usa","uk")
     lateinit var tvDataReceived: TextView
     lateinit var etHome: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)  //inflating the layout xml
         etHome = findViewById(R.id.etHome)
+
+        var adapter = ArrayAdapter<String>(this,
+                                    android.R.layout.simple_list_item_1,
+                                    countries)
+        var countriesSpinner = findViewById<Spinner>(R.id.spinnerCountries)
+        countriesSpinner.adapter = adapter
+
         /*tvDataReceived = findViewById(R.id.tv_dataReceived) //taking handle on the textview
         var receivedData =  intent.getStringExtra("mykey")
         tvDataReceived.text = receivedData*/
