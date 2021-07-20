1677f191cfd2fda0016a5e90cf7be6ff4647f3b7
abdul <abdul.tanveer@gmail.com>
2021-07-20 16:36:50 +0530

    options menu

--- app/src/main/java/com/abdul/myfirsttap/HomeActivity.kt
+++ app/src/main/java/com/abdul/myfirsttap/HomeActivity.kt
@@ -3,12 +3,12 @@ package com.abdul.myfirsttap
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 
 class HomeActivity : AppCompatActivity() {
     var TAG = HomeActivity::class.simpleName
@@ -19,6 +19,7 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+       // var name = savedInstanceState?.getString("name")
         setContentView(R.layout.activity_home)  //inflating the layout xml
         etHome = findViewById(R.id.etHome)
         Log.i(TAG,"onCreate")
@@ -73,4 +74,28 @@ class HomeActivity : AppCompatActivity() {
         setResult(RESULT_OK,dataIntent)
         finish()
     }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString("name","abdul")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         super.onCreateOptionsMenu(menu)
+        var menuInflater = menuInflater
+        menuInflater.inflate(R.menu.home_activity_menu,menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+         super.onOptionsItemSelected(item)
+        when(item.itemId){
+            R.id.fItem -> {Toast.makeText(this,"first item",Toast.LENGTH_SHORT).show()}
+            R.id.sItem -> {Toast.makeText(this,"second item",Toast.LENGTH_SHORT).show()}
+
+        }
+        return true
+    }
+
 }
