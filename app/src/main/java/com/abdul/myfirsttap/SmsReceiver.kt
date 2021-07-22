b63e4c67a6a1d387f78b43004aa4c5f1a5328d8b
abdul <abdul.tanveer@gmail.com>
2021-07-22 13:59:30 +0530

    SmsReceiver.kt -- getting sms body

--- app/src/main/java/com/abdul/myfirsttap/SmsReceiver.kt
+++ app/src/main/java/com/abdul/myfirsttap/SmsReceiver.kt
@@ -3,10 +3,33 @@ package com.abdul.myfirsttap
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.SmsMessage
 import android.widget.Toast
 
+
 class SmsReceiver: BroadcastReceiver() {
-    override fun onReceive(context: Context?, p1: Intent?) {
-        Toast.makeText(context,"abdul u 've an sms",Toast.LENGTH_SHORT).show()
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        if (intent!!.action.equals("android.provider.Telephony.SMS_RECEIVED")) {
+            val bundle = intent!!.extras //---get the SMS message passed in---
+            var msgs: Array<SmsMessage?>? = null
+            var msg_from: String
+            if (bundle != null) {
+                //---retrieve the SMS message received---
+                try {
+                    val pdus = bundle["pdus"] as Array<Any>?
+                    msgs = arrayOfNulls<SmsMessage>(pdus!!.size)
+                    for (i in msgs.indices) {
+                        msgs!![i] = SmsMessage.createFromPdu(pdus!![i] as ByteArray)
+                        msg_from = msgs[i]?.getOriginatingAddress().toString()
+                        val msgBody: String = msgs[i]?.getMessageBody().toString()
+                        Toast.makeText(context, msgBody, Toast.LENGTH_SHORT).show()
+
+                    }
+                } catch (e: Exception) {
+//                            Log.d("Exception caught",e.getMessage());
+                }
+            }
+        }
     }
 }
